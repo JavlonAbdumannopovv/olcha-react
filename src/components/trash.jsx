7c984160e80589944d7ab3bab0f37e66a75531dc
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-06-11 22:39:42 +0500

    mock data full completed

--- src/components/trash.jsx
+++ src/components/trash.jsx
@@ -17,17 +17,15 @@ function trash() {
     }
   }
   
-  class Smartfon extends Product {
-    constructor(img, model, brend, cost, color, ram, memory, system) {
+  class Rul extends Product {
+    constructor(img, model, brend, cost, color) {
       super(img, model, brend, cost, color);
-      this.gCategory = "Smartfon, telefon, gadjet, aksessuarlar";
-      this.Category = "Smartfonlar";
-      this.type = "Smartfon";
-      this.ram = ram;
-      this.memory = memory;
-      this.operationSystem = system;
+      this.gCategory = "Avto jihozlar";
+      this.Category = "Avto uchun rul";
+      this.type = "Rul";
+      this.description = "Siz ushbu mahsulotni bizdan rasmiy kafolat bilan eng arzon narxda xarid qilishingiz mumkin. Tovarlarni bo'lib-bo'lib va kreditsiz xarid qilish imkoniyati mavjud. Yetkazib berish xizmati mavjud. O'zbekiston bo'ylab tez yetkazib berish. Biz bilan tez va oson xarid qiling. Biz siz uchun faqat eng yaxshisini tanladik, shuning uchun endi siz do'konlar bo'ylab yugurishingiz va qidirishga ko'p vaqt sarflashingiz shart emas. Sizga kerak bo'lgan hamma narsa bizda!"
     }
-  } shu classdan foydalanib 10ta  obyektyasab ber cost xususiyatiga 7 xonali sonlarni taminla yani uzbek somiga aylantir
+  }shu classdan foydalanib 10ta  haqiqiy obyekt yasab ber cost xususiyatiga 7 xonali sonlarni taminla yani uzbek somiga aylantir
   return (
     <div>
       
